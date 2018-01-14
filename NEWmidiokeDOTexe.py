df608daa384fd26209078741a75f85b6bee5e1cd
Geerten Verweij <g.verweij@eaglescience.nl>
2018-01-14 12:11:25 +0100

    removed some comments

--- NEWmidiokeDOTexe.py
+++ NEWmidiokeDOTexe.py
@@ -55,19 +55,10 @@ def getFrequencies(sig, fs):
 	ready made fundemental frequency detection method that works much better
 	than what we tried manually with fft and autocorrelation
 	"""
-	# Calculate autocorrelation (same thing as convolution, but with
-	# one input reversed in time), and throw away the negative lags
 	corr = fftconvolve(sig, sig[::-1], mode='full')
 	corr = corr[len(corr)//2:]
-
-	# Find the first low point
 	d = np.diff(corr)
 	start = find(d > 0)[0]
-
-	# Find the next peak after the low point (other than 0 lag).  This bit is
-	# not reliable for long signals, due to the desired peak occurring between
-	# samples, and other peaks appearing higher.
-	# Should use a weighting function to de-emphasize the peaks at longer lags.
 	peak = np.argmax(corr[start:]) + start
 	px, py = parabolic(corr, peak)
 
